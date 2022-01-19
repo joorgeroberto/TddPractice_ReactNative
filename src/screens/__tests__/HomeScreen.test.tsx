3ae1ff9f10582f0a1b26a97d3c794a71bd70f518
Jorge Roberto <jorgercj@dcomp.ufs.br>
2022-01-18 22:37:46 -0300

    feat: adding date and day components

--- src/screens/__tests__/HomeScreen.test.tsx
+++ src/screens/__tests__/HomeScreen.test.tsx
@@ -7,4 +7,25 @@ describe('HomeScreen', () => {
     const wrapper = render(<HomeScreen />);
     wrapper.getByTestId('home-screen');
   });
+
+  describe('Title description', () => {
+    beforeEach(() => {
+      jest.useFakeTimers('modern');
+      jest.setSystemTime(946692000000); // Saturday, 01 January 2000 00:00 UTC
+    });
+
+    afterEach(() => {
+      jest.useRealTimers();
+    });
+
+    test('Should contain current date', () => {
+      const wrapper = render(<HomeScreen />);
+      wrapper.getByText('Jan 01, 2000');
+    });
+
+    test('Should contain current day', () => {
+      const wrapper = render(<HomeScreen />);
+      wrapper.getByText('Saturday');
+    });
+  });
 });
