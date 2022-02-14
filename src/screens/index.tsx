df5f035c5b5bb4bc0f2e138ba854f8006828b4cf
Jorge Roberto <jorgercj@dcomp.ufs.br>
2022-02-14 01:00:16 -0300

    feat: navigation tests workinggit add .

--- src/screens/index.tsx
+++ src/screens/index.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
 import HomeScreen from './HomeScreen';
+import WeatherScreen from './WeatherScreen';
 
 export type RootStackParamList = {
   Home: undefined;
+  Weather: { latitude: number; longitude: number };
 };
 
 const RootStack = createStackNavigator<RootStackParamList>();
@@ -14,6 +16,7 @@ function AppNavigator() {
     <NavigationContainer>
       <RootStack.Navigator screenOptions={{ headerShown: false }}>
         <RootStack.Screen name="Home" component={HomeScreen} />
+        <RootStack.Screen name="Weather" component={WeatherScreen} />
       </RootStack.Navigator>
     </NavigationContainer>
   );
